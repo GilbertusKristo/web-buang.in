119dd51c56b69a14b9fe6650228c020b508a38c5
GilbertusKristo <112887683+GilbertusKristo@users.noreply.github.com>
2025-05-17 00:38:13 +0700

    resolve error

--- resources/views/pages/auth/auth-login.blade.php
+++ resources/views/pages/auth/auth-login.blade.php
@@ -1,51 +1,9 @@
 <!DOCTYPE html>
 <html lang="id">
-<<<<<<< HEAD
-
-=======
->>>>>>> 515a80e (update landing page)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Masuk - Buang.in</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</head>
-
-<body>
-    <div class="container">
-        <div class="auth-card">
-            <div class="logo">
-                <img src="{{ asset('img/buangin.png') }}" alt="Buang.in">
-            </div>
-
-            <h1>Masuk Ke Akun Anda</h1>
-            <p class="subtitle">Masukkan Username dan Password anda</p>
-
-            <form method="POST" action="{{ route('web.auth.login') }}">
-                @csrf
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" id="email" name="email" required>
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" id="password" name="password" required>
-                    <div class="forgot-password">
-                        {{-- <a href="{{ route("#") }}">Lupa Password?</a> --}}
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary">Masuk</button>
-                <div class="auth-footer">
-                    <p>Belum punya akun? <a href="{{ route('auth.register.form') }}">Daftar sekarang</a></p>
-                </div>
-            </form>
-        </div>
-    </div>
-</body>
-
-</html>
-=======
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
@@ -151,4 +109,3 @@
     </div>
 </body>
 </html>
->>>>>>> 515a80e (update landing page)
